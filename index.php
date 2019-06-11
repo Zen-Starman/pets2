34be6117b0269bc34b7570d92a8804e3277aacae
Zane Stearman <zstearman@mail.greenriver.edu>
2019-06-11 02:52:29 -0400

    update index

--- index.php
+++ index.php
@@ -29,7 +29,27 @@ echo "<h2> My Pets</h2><br>
 
 $f3->route('GET /@animal', function($f3, $params)
 {
+    $animal = $params['animal'];
 
+    switch ($animal) {
+        case 'dog':
+            echo "<h3>Woof!</h3>";
+            break;
+        case 'cat':
+            echo "<h3>Meow</h3>";
+            break;
+        case 'Chicken':
+            echo "<h3>Cluck!</h3>";
+            break;
+        case 'Snake':
+            echo "<h3>SSSssssS!</h3>";
+            break;
+        case 'Cow':
+            echo "<h3>MooO</h3>";
+            break;
+        default:
+            $f3->error(404);
+        }
 });
 
 $f3->route('GET /order', function(){
